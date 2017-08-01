1e470ce4c404f4794e8c3da70791ed6f0450c9aa
ibmchandu <30441377+ibmchandu@users.noreply.github.com>
2017-08-01 13:42:10 +0530

    initial commit

---
+++ src/com/git/example/GitDemo.java
@@ -0,0 +1,12 @@
+package com.git.example;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		getData();
+		
+	}
+	public static void getData(){
+		System.out.println("Hello Git Application");
+	}
+
+}
