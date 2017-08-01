96ba2e577b8221dbe3a19df237d5a5e32bfed3d2
ibmchandu <30441377+ibmchandu@users.noreply.github.com>
2017-08-01 14:28:40 +0530

    second commit

---
+++ src/com/git/example/Git.java
@@ -0,0 +1,20 @@
+package com.git.example;
+
+public class Git {
+	private int git_id;
+	private String name;
+	public int getGit_id() {
+		return git_id;
+	}
+	public void setGit_id(int git_id) {
+		this.git_id = git_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
